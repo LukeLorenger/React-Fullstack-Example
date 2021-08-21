731045f8f6eb9e9ca99513e1e913202968bff666
Luke Lorenger <dirtydeadtv@gmail.com>
2021-08-20 22:48:27 -0400

    React-Chat-Engine Added

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import '../../App.css'
+import '../../App.css';
+import { ChatEngine } from 'react-chat-engine';
 
+const { REACT_APP_PROJECTID, REACT_APP_USERNAME, REACT_APP_USERSECRET} = process.env
+
+var projId = REACT_APP_PROJECTID
+var userName = REACT_APP_USERNAME
+var secret = REACT_APP_USERSECRET
 function Home() {
     return (
-        <div>
-        <h1>Home</h1>
-        </div>
-        // <>
-         
-        // </>
+        <ChatEngine
+			projectID='b2e79a73-9fd7-4d45-9e65-06a70bbf3822'
+			userName='firstTestUser'
+			userSecret='123123'
+		/>
     )
 }
 
