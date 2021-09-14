3540718e06b8d29dccee5593cb484c1b3d6d4a71
Luke Lorenger <dirtydeadtv@gmail.com>
2021-09-14 11:32:42 -0400

    Services page adjustments

--- client/src/components/pages/Services.js
+++ client/src/components/pages/Services.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import './Services.css';
 
 function Services() {
     return (
         <div>
-        <h1>Services</h1> 
+        <h1>Services</h1>
+        <h2>Web Development</h2>
+        <h3>Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, 
+        Lorem Ipsum, Lorem Ipsum,Lorem Ipsum, Lorem Ipsum,
+        Lorem Ipsum, Lorem Ipsum,Lorem Ipsum, Lorem Ipsum,</h3>
+        <h2>Game Development</h2>
+        <h3>Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, 
+        Lorem Ipsum, Lorem Ipsum,Lorem Ipsum, Lorem Ipsum,
+        Lorem Ipsum, Lorem Ipsum,Lorem Ipsum, Lorem Ipsum,</h3>
+        <h2>Cloud</h2>
+        <h3>Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, 
+        Lorem Ipsum, Lorem Ipsum,Lorem Ipsum, Lorem Ipsum,
+        Lorem Ipsum, Lorem Ipsum,Lorem Ipsum, Lorem Ipsum,</h3>
+        <h2>IoT</h2>
+        <h3>Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, 
+        Lorem Ipsum, Lorem Ipsum,Lorem Ipsum, Lorem Ipsum,
+        Lorem Ipsum, Lorem Ipsum,Lorem Ipsum, Lorem Ipsum,</h3>
+        <h2>Desktop</h2>
+        <h3>Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, Lorem Ipsum, 
+        Lorem Ipsum, Lorem Ipsum,Lorem Ipsum, Lorem Ipsum,
+        Lorem Ipsum, Lorem Ipsum,Lorem Ipsum, Lorem Ipsum,</h3>
         </div>
     )
 }
